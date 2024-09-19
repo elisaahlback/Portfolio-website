7288ad08f957840cab54e2a272a0cb101e57b312
norris <norris@student.chalmers.se>
2024-09-19 12:36:31 +0200

    Refactor: Update structure of main page

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -6,23 +6,20 @@ const NavBar = () => {
     return (
     <nav class="nav">
         <div class="container">
-        <b-row class="row justify-content-md-center">
-            <b-col class="nav-title">
-                <a href="/" class="nav-title col-12">elisa ahlbäck</a>
+        <b-row class="row">
+            <b-col class="nav-title col-2 d-flex align-items-center">
+                <a href="/" class="nav-logo"><img id="nav-logo" src={require('.././assets/nav-logo.png')} /></a>
             </b-col>
+            
+            <div class="nav-item1 col col-2 d-flex justify-content-center align-items-center"><a href="/">about me</a>
+            </div>
+            <div class="nav-item2 col col-2 d-flex justify-content-center align-items-center"><a href="/Projects">projects</a>
+            </div>
+            <div class="nav-item3 col col-2 d-flex justify-content-center align-items-center"><a href="/Contact">contact</a>
+            </div>
         </b-row>
 
-        <div class="row justify-content-md-center">
-    <div class="nav-item1 col col-2">
-    <a href="/">about me</a>
-    </div>
-    <div class="nav-item2 col col-2">
-        <a href="/Projects">projects</a>
-    </div>
-    <div class="nav-item3 col col-2">
-        <a href="/Contact">contact</a>
-    </div>
-  </div>
+        
         </div>
         
     </nav>
--- my-app/src/routes/MainPage.js
+++ my-app/src/routes/MainPage.js
@@ -4,94 +4,41 @@ import NavBar from '.././components/NavBar';
 import FigmaLogo from '.././assets/figma.svg';
 import Line from '.././components/Line';
 import Footer from '.././components/Footer';
+import { Link, Element } from 'react-scroll';
 
 function MainPage() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <div class="row hero-row d-flex align-items-center">
-        <div class="col col-7 ">
-          <h1 class="hero">
-            A software engineering student, with a passion for front-end development and UX.
+        <div class="col col-12 ">
+          <h1 class="name col-3">
+             elisa ahlbäck
           </h1>
         </div>
       </div>
-        
-      <div class="row aboutme-row d-flex align-items-center">
-        <div class="col col-5">
-        <img id="profile-pic" src={require('.././assets/profile.png')} />
+
+      <div className="about-me-rectangle">
+      <div className="container-fluid h-100">
+      <div className="row h-100">
+        <div className="col-6 about-me-left d-flex justify-content-center align-items-center">
+        <h1 class="about-me-left-text">
+            A software engineering graduate, with a passion for front-end development and UX.
+          </h1>
         </div>
-
-        <div class="col col-7">
-
-          <div class="row">
-            <div class="col">
-              <h3 id="intro-title">Hi, I'm Elisa!</h3>
-            </div>
-          </div>
-
+        <div className="col-6 about-me-right">
           <div class="row">
             <div class="col">
               <p>A half-English, half-Swedish creative thinker, passionate about design and creating things?. After growing up in Spain, I moved to Sweden to pursue a Software Engineering & Management bachelors degree, which I’m currently in the second year.</p><br/>
               <p>I strive to continuously improve and gain further experience, as well as enjoying learning from others around me, being easily adaptable to a team or individual work environments.</p>
             </div>
           </div>
-
-          <div class="row">
-            <div class="col">
-              <img id="location-icon" src={require('.././assets/location.png')} />
-              <p><b>Gothenburg, Sweden</b></p>
-            </div>
-          </div>
-
-        </div>
-
-        <div class="container-fluid">
-        <div class="row tools-row d-flex align-items-center">
-        <div class="col col-12 d-flex justify-content-center">
-          <div class="d-flex justify-content-center" id="toolbox">
-            <div class="row tools-row d-flex align-items-center justify-content-center">
-              
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/html.png')}></img>
-              </div>
-              
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/css.png')}></img>
-              </div>
-
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/javascript.webp')}></img>
-              </div>
-
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/java.png')}></img>
-              </div>
-
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/vue.png')}></img>
-              </div>
-
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/bootstrap.png')}></img>
-              </div>
-
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={require('.././assets/git.png')}></img>
-              </div>
-
-              <div class="col col-1 d-flex justify-content-center">
-                <img id="tool-icon" src={FigmaLogo} alt="Figma Logo"></img>
-              </div>
-
-            </div>
-  
-          </div>
         </div>
       </div>
       </div>
-
       </div>
+        
+      
 
       <div class="row projects-row">
         <div class="row project-row">
